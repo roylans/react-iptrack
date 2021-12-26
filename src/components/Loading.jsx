2388695cbccb0ce9299bc01189fe7d535be5db4e
Roylan Suarez <roylan.suarez@gmail.com>
2021-12-26 11:38:03 -0500

    feat: Add component Loading

---
+++ src/components/Loading.jsx
@@ -0,0 +1,11 @@
+import style from '../css/main.module.scss'
+
+function Loading() {
+    return (
+        <div className={style.loading}>
+            Loading
+        </div>
+    )
+}
+
+export default Loading
